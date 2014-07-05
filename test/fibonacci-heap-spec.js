5eaef801c21901e12285a439f5f1c2a47fb2624e
Tyriar <tyriar@tyriar.com>
2014-07-05 10:49:33 -0700

    Add Fibonacci heap tests
    Fixes #21

--- test/fibonacci-heap-spec.js
+++ test/fibonacci-heap-spec.js
@@ -3,4 +3,64 @@ var FibonacciHeap = require("../src/fibonacci-heap");
 
 describe("fibonacci-heap", function () {
   HeapTests(FibonacciHeap);
+
+  describe("calling insert 1000 times", function () {
+    var heap;
+
+    beforeEach(function () {
+      heap = new FibonacciHeap();
+      for (var i = 0; i < 1000; i++) {
+        heap.insert(i, i);
+      }
+    });
+
+    it("should have a nodelist of length 1000", function () {
+      var count = 0;
+      var node = heap.minNode;
+      do {
+        count++;
+        node = node.next;
+      } while (node !== heap.minNode);
+      expect(count).toBe(1000);
+    });
+
+    it("should have a nodelist of length 1000 (searching backwards)", function () {
+      var count = 0;
+      var node = heap.minNode;
+      do {
+        count++;
+        node = node.prev;
+      } while (node !== heap.minNode);
+      expect(count).toBe(1000);
+    });
+
+    it("should have a nodelist with no parents or children", function () {
+      var childCount = 0;
+      var parentCount = 0;
+      var node = heap.minNode;
+      do {
+        if (node.child) {
+          childCount++;
+        }
+        if (node.parent) {
+          parentCount++;
+        }
+        node = node.next;
+      } while (node !== heap.minNode);
+      expect(childCount).toBe(0);
+      expect(parentCount).toBe(0);
+    });
+
+    it("should contain correct key-value pairs", function () {
+      var invalidCount = 0;
+      var node = heap.minNode;
+      do {
+        if (node.key !== node.value) {
+          invalidCount++;
+        }
+        node = node.prev;
+      } while (node !== heap.minNode);
+      expect(invalidCount).toBe(0);
+    });
+  });
 });
